c5765e9272d7dd11b03350db41fcceeed17589fc
Augusto-logo <augusto2.logo@gmail.com>
2025-05-26 21:37:38 -0300

    .

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -11,12 +11,14 @@ from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
+from fast_zero.settings import Settings
 
+settings = Settings()  # type: ignore
 SECRET_KEY = 'your-secret-key'  # Isso é provisório, vamos ajustar!
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_context = PasswordHash.recommended()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 def create_access_token(data: dict):
