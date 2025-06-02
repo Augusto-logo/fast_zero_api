237edaa1af7e4a7591ee974dc0938cee07fb19cf
Augusto-logo <augusto2.logo@gmail.com>
2025-06-01 21:41:00 -0300

    .

--- alembic/env.py
+++ alembic/env.py
@@ -1,17 +1,24 @@
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from alembic import context
 
-from fast_zero.models import table_registry
-from fast_zero.settings import Settings
-
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', 'postgresql+psycopg2://postgres:123456@localhost:5432/teste')
 
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = table_registry.metadata
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -43,26 +50,22 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def do_run_migrations(connection):
-    context.configure(connection=connection, target_metadata=target_metadata)
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
 
-    with context.begin_transaction():
-        context.run_migrations()
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
 
-
-
-
-def run_migrations_online():
+    """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
+            connection=connection, target_metadata=target_metadata
         )
 
         with context.begin_transaction():
--- fast_zero/database.py
+++ fast_zero/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-
 from fast_zero.settings import Settings
 
-engine = create_async_engine(Settings().DATABASE_URL)
+
+# engine = create_async_engine(Settings().DATABASE_URL) # type: ignore
+
+engine = create_async_engine("postgresql+asyncpg://postgres:123456@localhost:5432/teste", echo=True)
 
 
 async def get_session():
